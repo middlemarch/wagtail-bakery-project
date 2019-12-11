acf4fd969110d0041d7965942821e8faf017112f
Rachel Flynn <rachel.flynn@propylon.com>
2019-12-11 16:15:35 +0000

    aws support

--- bakerydemo/settings/production.py
+++ bakerydemo/settings/production.py
@@ -95,11 +95,12 @@ AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_AUTO_CREATE_BUCKET = True
 
+AWS_LOCATION = 'static'
 INSTALLED_APPS.append('storages')
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'bakerydemo.storage_backends.MediaStorage'
 
 if 'GS_BUCKET_NAME' in os.environ:
     GS_BUCKET_NAME = os.getenv('GS_BUCKET_NAME')
