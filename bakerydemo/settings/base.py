57fa1b11d24281575dec2df4ebc7f64c2a5b2681
Rachel Flynn <rachel.flynn@propylon.com>
2019-12-12 12:14:01 +0000

    add admin recaptcha

--- bakerydemo/settings/base.py
+++ bakerydemo/settings/base.py
@@ -78,6 +78,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     'captcha',
+    'captcha_admin',
     'wagtailcaptcha',
 ]
 
--- bakerydemo/urls.py
+++ bakerydemo/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.conf import settings
-from django.contrib import admin
+from captcha_admin import admin
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
